c86da06728b11d1264d47977f0d5041a258f52a4
ME1O <1miyagitop3@gmail.com>
2024-12-04 19:59:05 +0300

    Добавлен вывод суммы двух чисел

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include "sdt.h"
+#include <iostream>
+using namespace std;
 
 int main() {
+    int a, b;
+    cout << "Введите два числа: ";
+    cin >> a >> b;
+    cout << "Сумма: " << a + b << endl;
     return 0;
 }
